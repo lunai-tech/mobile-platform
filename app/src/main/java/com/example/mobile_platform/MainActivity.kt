7f5f172aef6748e87be1788cb6536d752341c842
Cesar Rodrigues <cesar.rodbr@gmail.com>
2021-08-23 21:04:37 -0300

    feature/criando menu de navegação

--- app/src/main/java/com/example/mobile_platform/MainActivity.kt
+++ app/src/main/java/com/example/mobile_platform/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_experts, R.id.navigation_trilha, R.id.navigation_meu_negocio
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
